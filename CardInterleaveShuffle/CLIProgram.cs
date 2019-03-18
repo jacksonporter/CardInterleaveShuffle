edea705bf8ea1136125789f1f2019aee6e6b4346
Jackson Porter <jackson_porter@outlook.com>
2019-03-18 11:24:07 -0600

    Removed explanation at the top of file.

--- CardInterleaveShuffle/CLIProgram.cs
+++ CardInterleaveShuffle/CLIProgram.cs
@@ -1,24 +1,3 @@
-﻿/*
- Simple: 52 playing cards interleave shuffle -
-
-Generate a deck of 52 unique playing cards in random order.  -
-
-Do an interleave shuffle. 
-
-ie: split the cards into 2 decks, and interleave them. 
-
-- Draw a 2nd top card, and draw a second bottom card. 
-Print these cards on the screen. 
-
-An example deck of 6 cards:  
-
-Input deck: 6 4 2 5 3 1 
-Cut into 2 decks: 6 4 2 | 5 3 1 
-Interleave them:   6   6 5   6 5 4   6 5 4 3   6 5 4 3 2   6 5 4 3 2 1 
-
-Deck generated from the shuffle function: 6 5 4 3 2 1 
-Print out "Top+1 = 5, Bottom-1 = 2" */
-
 using System;
 using System.Collections.Generic;
 
